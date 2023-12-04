cd654181313ce6226b1f77557c009f53cd493596
Kieran <kieran.bieniosek@swyfft.com>
2023-12-03 20:05:16 -0800

    switch to simpler modulus instead of montgomery space

--- primetest.py
+++ primetest.py
@@ -1,7 +1,10 @@
 import concurrent.futures
-from gmpy2 import mpz
+from gmpy2 import mpz, f_divmod_2exp, f_mod_2exp, bit_length, gcd
+import gmpy2
+import itertools
 import random
 import time
+import math
 
 def sq_adj(x, adj=0):
     return x * x + adj
@@ -15,9 +18,17 @@ def just_div(x, n):
 def just_mult(a, b):
     return a * b
 
-def just_mod_2(x, n):
-    """n must be power of 2"""
-    return x & (n - 1)
+def just_mult1(a, b):
+    return a * b
+
+def just_mult2(a, b):
+    return a * b
+
+def just_mult3(a, b):
+    return a * b
+
+def just_mod_bits(x, nbits):
+    return f_mod_2exp(x, nbits)
 
 def div_bits(x, nbits):
     return x >> nbits
@@ -28,11 +39,12 @@ def sq_mod_n(x, n, adj=0):
 def mul_mod_n(x, a, n):
     return (x * a) % n
 
-def powmod(a, n):
-    """Compute a^n (mod n)"""
+
+def powmod(a, x, n):
+    """Compute a^x (mod n)"""
     running_total = a
     binary_representation = []
-    i = n
+    i = x
     while i > 0:
         binary_representation.insert(0, i % 2)
         i = i // 2
@@ -45,25 +57,85 @@ def powmod(a, n):
     
     return prev_total
 
+def powmod2(a, x, n):
+    return powmod(a, x, n)
+
+KNOWN_M = {}
+def pollard_factor(n, a=3, b=100):
+#    else:
+#        m = 1
+#        for q in range(3, b + 1):
+#            if isprime(q):
+#                m *= pow(q, math.floor(math.log(b, q)))
+#                m *= q
+#        k = a ** m - 1
+    if b in KNOWN_M:
+        m = KNOWN_M[b]
+    else:
+        m = 1
+        factors = []
+        for q in itertools.chain([2], range(3, b + 1, 2)):
+            if isprime(q):
+                factors.append(pow(q, int(math.log(b, q))))
+        for factor in factors:
+            m *= factor
+        KNOWN_M[b] = m
+    k = powmod2(a, m, n) - 1
+    g = gcd(k, n)
+    if g == n and b > 5:
+        return pollard_factor(n, b=b//2)
+    else:
+        return g
+
 def isprime(n):
     # uses fermat's little theorem
     if n <= 3:
         return True
-    aa = random.choices(range(1, min(10000, n - 1)), k=5)
-    prev = [powmod(a, n) for a in aa]
+    elif n % 2 == 0:
+        return False
+    aa = range(2, min(n, 10))
+    prev = [powmod(a, n, n) for a in aa]
     return all([p in (-1, 1) for p in prev])
+
+def reduce_modulus(s, p):
+    s_bottom, s_top = f_divmod_2exp(s, p)
+    return s_bottom + s_top
+
+def just_eq(a, b):
+    return a == b
+
+def just_sub(a, b):
+    return a - b
+
+def lucaslehmer(p):
+    # uses lucas lehmer primality test for candidate primes 2^p - 1
+    start = time.time()
+
+    s = 4
+    for i in range(p - 2):
+        s = gmpy2.square(s)
+        s = gmpy2.sub(s, 2)
+        while bit_length(s) > p:
+            s = reduce_modulus(s, p)
+        if gmpy2.bit_scan0(s) == p:
+            s = 0
+    return s == 0, time.time() -  start, False
 
 def lucaslehmer_montgomery(p):
     # uses lucas lehmer primality test for candidate primes 2^p - 1
     start = time.time()
+
     n = (1 << p) - 1
+#    factor = pollard_factor(mpz(n), b=2 + p)
+#    if n > factor > 1:
+#        print(f"factor 2^{p}-1: {factor}")
+ #       return False, time.time() - start, True
     r, rbits = next_power_2(n)
     s = Montgomery(4, r, n, rbits)
     for i in range(p - 2):
         s.mult(s)
         s.add(-2)
-#        print(n, i, s)
-    return s.to_int() == 0, time.time() - start
+    return s.to_int() == 0, time.time() -  start, False
 
 def extended_gcd(a, b):
     s = 0
@@ -97,15 +169,30 @@ class Montgomery:
         return from_montgomery_form(self.m, self.rprime, self.n)
     
     def redc(self, t):
-        m = just_mod_2(just_mult(just_mod_2(t, self.r), self.nprime), self.r)
-        t = div_bits((t + just_mult(m, self.n)), self.rbits)
+        m = just_mod_bits(just_mult2(just_mod_bits(t, self.rbits), self.nprime), self.rbits)
+        t = div_bits((t + just_mult3(m, self.n)), self.rbits)
         if t >= self.n:
             return t - self.n
         else:
             return t
+        
+#    def multi_redc(self, t, word_length=8):
+#        """T is little-endian"""
+#        rwords = math.ceil(self.rbits / word_length)
+ #       nwords = math.ceil(self.nbits / word_length)
+  #      word_bits = (1 << word_length) - 1
+   #     t_array = [0] * (rwords + nwords)
+    #    for i in range(nwords):
+     #       t_array[i] = nwords >> (i * word_length) & ((1 << word_length) - 1)
+      #  for i in range(rwords):
+#            c = 0
+#            m = just_mod_bits(t_array[i] * self.nprime, self.word_bits)
+#            for j in range(nwords):
+#                x = t_array[i + j] + m * 
+
 
     def mult(self, a):
-        self.m = self.redc(just_mult(self.m, a.m))
+        self.m = self.redc(just_mult1(self.m, a.m))
 
     def add(self, x):
         self.m += x
@@ -126,32 +213,46 @@ class FakeFuture:
         self.i = i
 
     def result(self):
-        start = time.time()
         return lucaslehmer_montgomery(self.i)
 
 def mersennes(start, stop, use_threads=True):
     cpu_elapsed = 0
     start_time = time.time()
     results = []
+    skips = 0
+    count = 0
+    last_output = time.time()
     if use_threads:
         with concurrent.futures.ProcessPoolExecutor() as executor:
             for i in prime_candidates(start, stop):
-                future = executor.submit(lucaslehmer_montgomery, i)
+                future = executor.submit(lucaslehmer, i)
                 results.append((i, future))
-            print("Submitted")
+            print(f"Submitted (start={start}, stop={stop})")
+            for i, future in results:
+                q, elapsed, skipped = future.result()
+                cpu_elapsed += elapsed
+                if q:
+                    print(f"2^{i}-1", q)
+                    last_output = time.time()
+                elif time.time() > last_output + 10:
+                    print(f" ({i})")
+                    last_output = time.time()
+                count += 1
+                if skipped:
+                    skips += 1
+            print("Wall clock", time.time() - start_time, "cpu time", cpu_elapsed)
     else:
         for i in prime_candidates(start, stop):
-            future = FakeFuture(i)
-            results.append((i, future))
-
-    for i, future in results:
-        q, elapsed = future.result()
-        cpu_elapsed += elapsed
-        if q:
-            print(f"2^{i}-1", q)
-        elif q % 100 == 0:
-            print(f" (i)")
-    print("Wall clock", time.time() - start_time, "cpu time", cpu_elapsed)
+            q, elapsed, skipped = lucaslehmer(i)
+            if q:
+                print(f"2^{i}-1", q)
+            elif time.time() > last_output + 10:
+                print(f" ({i})")
+                last_output = time.time()
+            count += 1
+            if skipped:
+                skips += 1
+    print(f"Skipped: {skips}/{count}")
 
 def next_power_2(n):
     x = 1
@@ -164,6 +265,7 @@ def next_power_2(n):
 if __name__ == '__main__':
     import cProfile
     start = time.time()
-    cProfile.run("mersennes(30000, 80000, use_threads=True)", sort="cumtime")
+    cProfile.run("mersennes(1000, 5000, use_threads=False)", sort="cumtime")
+#    mersennes(50000, 51000)
     print("elapsed", time.time() - start)
 
